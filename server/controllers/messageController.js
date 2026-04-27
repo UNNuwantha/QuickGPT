101cde8d1194522ab75fc4df62172e4fd4421c03
UNNuwantha <www.nadun50@gmail.com>
2026-04-27 16:55:07 +0530

    Developed the  text and  Image generation features

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,11 +1,20 @@
+import axios from "axios"
 import Chat from "../models/Chat.js"
 import User from "../models/User.js"
+import imagekit from "../configs/imageKit.js"
+import openai from "../configs/openai.js"
 
 
 // Text-based AI Chat Message Controller
 export const textMessageController = async (req, res) => {
     try {
         const userId = req.user._id
+
+        //Check Credits
+        if (req.user.credits < 1) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
+        }
+
         const { chatId, prompt } = req.body
 
         const chat = await Chat.findOne({ userId, _id: chatId })
@@ -49,7 +58,35 @@ export const imageMessageController = async (req, res) => {
 
         //Push user message
         chat.messages.push({ role: "User", content: prompt, timestamp: Date.now(), isImage: false });
-    } catch (error) {
 
+        //Encode the prompt
+        const encodedPrompt = encodeURIComponent(prompt)
+
+        //Construct ImageKit AI generation URL 
+        const generateImageUrl = `${process.env.IMAGEKIT_URL_ENDPOINT}/ik-genimg-prompt-${encodedPrompt}/quickgpt/${Date.now()}.png?tr=w-800,h-800`;
+
+        //Trigger generation by fetching from ImageKit
+        const aiImageResponse = await axios.get(generateImageUrl, { responseType: "arraybuffer" })
+
+        //Convert to Base64
+        const base64Image = `data:image/png;base64,${Buffer.from(aiImageResponse.data, "binary").toString('base64')}`;
+
+        //Upload to imageKit Media Library
+        const uploadResponse = await imagekit.upload({
+            file: base64Image,
+            fileName: `${Date.now()}.png`,
+            folder: "quickgpt"
+        })
+
+        const reply = { role: 'assistant', content: uploadResponse.url, timestamp: Date.now(), isImage: true, isPublished }
+        res.json({ success: true, reply })
+
+        chat.messages.push(reply)
+        await chat.save()
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -2 } })
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
     }
 }
