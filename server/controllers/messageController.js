1f324a864deb9c81ee4b11f73d858f6066a909d9
UNNuwantha <www.nadun50@gmail.com>
2026-04-27 12:58:32 +0530

    Added openAI and ImageKit

---
+++ server/controllers/messageController.js
@@ -0,0 +1,55 @@
+import Chat from "../models/Chat.js"
+import User from "../models/User.js"
+
+
+// Text-based AI Chat Message Controller
+export const textMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id
+        const { chatId, prompt } = req.body
+
+        const chat = await Chat.findOne({ userId, _id: chatId })
+        chat.messages.push({ role: "User", content: prompt, timestamp: Date.now(), isImage: false })
+
+        const { choices } = await openai.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [
+                {
+                    role: "user",
+                    content: prompt,
+                },
+            ],
+        });
+
+        const reply = { ...choices[0].message, timestamp: Date.now(), isImage: false }
+        res.json({ success: true, reply })
+
+        chat.messages.push(reply)
+        await chat.save()
+
+        await User.updateOne({ _id: userId }, { $inc: { credits: -1 } })
+
+
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// Image Generation Message Controller
+export const imageMessageController = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        //Check credits
+        if (req.user.credits < 2) {
+            return res.json({ success: false, message: "You don't have enough credits to use this feature" })
+        }
+        const { prompt, chatId, isPublished } = req.body
+        //Find chat
+        const chat = await Chat.findOne({ userId, _id: chatId })
+
+        //Push user message
+        chat.messages.push({ role: "User", content: prompt, timestamp: Date.now(), isImage: false });
+    } catch (error) {
+
+    }
+}
