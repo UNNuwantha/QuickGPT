dc7501b248b66f891a424249f53f3198dffb0035
UNNuwantha <www.nadun50@gmail.com>
2026-04-28 06:11:41 +0530

    fixed the backend Issues

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -2,8 +2,31 @@ import Stripe from "stripe";
 import Transaction from "../models/Transaction.js";
 import User from "../models/User.js";
 
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+const processSession = async (session, response) => {
+    if (!session?.metadata) {
+        return response.json({ received: true, message: "Webhook received but session metadata missing" })
+    }
+
+    const { transactionId, appId } = session.metadata;
+
+    if (appId !== 'quickgpt') {
+        return response.json({ received: true, message: "Ignored event: Invalid app" })
+    }
+
+    const transaction = await Transaction.findOne({ _id: transactionId, isPaid: false })
+    if (!transaction) {
+        return response.json({ received: true, message: "Transaction not found or already processed" })
+    }
+
+    await User.updateOne({ _id: transaction.userId }, { $inc: { credits: transaction.credits } })
+    transaction.isPaid = true;
+    await transaction.save();
+    return response.json({ received: true })
+};
+
 export const stripeWebhooks = async (request, response) => {
-    const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET)
     const sig = request.headers["stripe-signature"]
 
     let event;
@@ -16,6 +39,11 @@ export const stripeWebhooks = async (request, response) => {
 
     try {
         switch (event.type) {
+            case "checkout.session.completed": {
+                const session = event.data.object;
+                return await processSession(session, response);
+            }
+
             case "payment_intent.succeeded": {
                 const paymentIntent = event.data.object;
                 const sessionList = await stripe.checkout.sessions.list({
@@ -23,28 +51,17 @@ export const stripeWebhooks = async (request, response) => {
                 })
 
                 const session = sessionList.data[0];
-                const { transactionId, appId } = session.metadata;
-
-                if (appId === 'quickgpt') {
-                    const transaction = await Transaction.findOne({ _id: transactionId, isPaid: false })
-
-                    //Update credits in user account
-                    await User.updateOne({ _id: transaction.userId }, { $inc: { credits: transaction.credits } })
-
-                    //Update credit payment status
-                    transaction.isPaid = true;
-                    await transaction.save();
-                } else {
-                    return response.json({ received: true, message: "Ignored event: Invalid app" })
+                if (!session) {
+                    return response.json({ received: true, message: "No checkout session found for payment intent" })
                 }
-                break;
+
+                return await processSession(session, response);
             }
 
             default:
-                console.log("Unhadled event type:", event.type)
-                break;
+                console.log("Unhandled event type:", event.type)
+                return response.json({ received: true, message: `Ignored event type: ${event.type}` })
         }
-        response.json({ received: true })
     } catch (error) {
         console.error("Webhook processing error: ", error)
         response.status(500).send("Internal Server Error")
